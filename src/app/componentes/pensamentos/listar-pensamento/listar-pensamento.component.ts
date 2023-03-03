41ee1548aa9cd1ef4e00b6c1eb7f43ec9cace23b
Robson Carvalho <uni.robson@gmail.com>
2023-03-03 15:26:12 -0300

    :rocket: Deploying stuff

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -9,35 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
-  listaPensamentos: Pensamento[] = [
-    // {
-    //   id: 1,
-    //   conteudo: 'Passo informações para componente filho.',
-    //   autoria: 'Componente pai',
-    //   modelo: 'modelo3'
-    // },
-    // {
-    //   id: 2,
-    //   conteudo: 'Minha propriedade é decorada com @Input()',
-    //   autoria: 'Componente filho',
-    //   modelo: 'modelo2'
-    // },
-    // {
-    //   id: 3,
-    //   conteudo: 'Você pode gerar um aplicativo a partir do seu código Angular usando o framework Ionic, NativeScript, Cordova, Capacitor e Electron. Isso permitirá que seja possível criar aplicativos móveis híbridos usando tecnologias web, como HTML, CSS e JavaScript. A escolha da plataforma depende das necessidades do projeto, das habilidades da equipe de desenvolvimento e dos requisitos de desempenho e escalabilidade.',
-    //   autoria: 'chatGPT',
-    //   modelo: 'modelo1'
-    // },
-  ]
+  listaPensamentos: Pensamento[] = []
+  paginaAtual: number = 1
+  haMaisPensamentos: boolean = true
+  filtro: string = ''
 
   constructor(private service: PensamentoService) { }
 
   ngOnInit(): void {
 
-    this.service.listar().subscribe((listaPensamentos) => {
+    this.service.listar(this.paginaAtual, this.filtro).subscribe((listaPensamentos) => {
       this.listaPensamentos = listaPensamentos
     })
+  }
 
+  carregarMaisPensamentos() {
+    this.service.listar(++this.paginaAtual, this.filtro)
+        .subscribe(listaPensamentos => {
+          this.listaPensamentos.push(...listaPensamentos)
+          if (!listaPensamentos.length) {
+              this.haMaisPensamentos = false
+          }
+        })
+  }
+
+  pesquisarPensamentos() {
+    this.haMaisPensamentos = true;
+    this.paginaAtual = 1;
+    this.service.listar(this.paginaAtual, this.filtro)
+      .subscribe((listaPensamentos) => {
+        this.listaPensamentos = listaPensamentos
+      })
   }
 
 }
